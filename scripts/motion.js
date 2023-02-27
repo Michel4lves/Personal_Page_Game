71b4b61dab59f394c798278fee963e0ca1983398
michel4alves <michelsantosa@gmail.com>
2023-02-27 02:17:06 -0300

    stairs new calc

--- scripts/motion.js
+++ scripts/motion.js
@@ -1,32 +1,56 @@
 var tracking = []
 var stairs = 0
 
-$(window).scroll(function(){
-    var scroll = $(window).scrollTop()
-        dh = $(document).height()
-        wh = $(window).height()
-        value = (scroll / (dh-wh)) * 100
-    $('.map').css('left', - value * 20 + '%')
-    
-    
-    // Main Personage Motion
-    $('.main_person').css('animation', 'runing 1s steps(6) infinite')
-
+function positionScreen() {
     var position = $('.map').offset().left
-        screenPosition = ($('.screen').offset().left) + position
+    screenPosition = ($('.screen').offset().left) + position
     tracking.push(screenPosition)
     if (tracking.length > 2) {
         tracking.shift()
     }
+}
 
+
+function characterDirection() {
+    positionScreen()
     if (tracking[0] > tracking[1]) {
         $('.main_person').css('transform', 'scaleX(1)')
     }else{
         $('.main_person').css('transform', 'scaleX(-1)')
     }
+}
 
 
-    // Laptop Motion
+function jumping() {
+    $('.main_person').addClass('jump')
+    setTimeout(() => {
+        $('.main_person').removeClass('jump')
+    },500)
+}
+
+
+function running() {
+    $('.main_person').addClass('running')
+    $('.main_person').removeClass('stopping')
+}
+
+
+$(window).scroll(function(){
+    var scroll = $(window).scrollTop()
+        dh = $(document).height()
+        wh = $(window).height()
+        value = (scroll / (dh-wh)) * 100
+    
+    $('.map').css('left', - value * 12 + '%')
+    $('.city-a').css('left', (value / 5) * 10 + '%')
+    $('.city-b').css('left', (value / 18) * 10 + '%')
+
+    
+    // Main Personage Motion
+    characterDirection()
+    running()
+
+    // Desktop, laptop, mobile Motion
     if (screenPosition <= -1100) {
         $('.desktop_a').css({animation: 'moving 1.5s linear both, walking_a 0.5s steps(2) infinite', visibility: 'hidden', transitionDelay: '1.5s'})
         $('.desktop_b').css({visibility: 'visible', transitionDelay: '1.7s'})
@@ -38,9 +62,8 @@ $(window).scroll(function(){
         $('.mobile_b').css({visibility: 'visible', transitionDelay: '2.1s', animationDelay: '1s'})
     }
 
-    // Ant / mail collect
-    if (screenPosition <= -2500) {
-        $('.ant').css({animation: 'appear 0.5s linear both'})
+    // Local, mail, cell collect
+    if (screenPosition <= -2890) {
         $('.local').css({animation: 'collect 0.2s linear both'})
         $('.local_no_collected').css({visibility: 'hidden'})
         $('.local_collected').css({visibility: 'visible'})
@@ -56,7 +79,7 @@ $(window).scroll(function(){
         $('.mobile_collected').css({visibility: 'visible'})
     }
 
-    // BREACH
+    // Breach
     if (screenPosition <= -3230 && screenPosition >= -3920) {
         $('.main_person').css('top', 'calc(100vh - 400px)')
     }else{
@@ -80,8 +103,9 @@ $(window).scroll(function(){
         $('.crowd_b2').css({animation: 'goal 0.5s linear infinite', animationDelay: '1s',  transitionDelay: '1s', visibility: 'visible'})
     }
 
+    // Skills
     if (screenPosition <= -4800) {
-        $('.knowlege').css({animation: 'classif 5s steps(6) both'}) 
+        $('.knowledge').css({animation: 'classif 5s steps(6) both'}) 
     }
     if (screenPosition <= -5300) {
         $('.frameworks').css({animation: 'classif 5s steps(6) both'}) 
@@ -90,113 +114,82 @@ $(window).scroll(function(){
         $('.learning').css({animation: 'classif 5s steps(6) both'}) 
     }
 
-
-
     // SUBWAY
-
     // Stairs
-    if (screenPosition <= -7850 && screenPosition >= -7960) {
-        $('.map').css('top', '-49px')
-    }else if (screenPosition <= -7961 && screenPosition >= -8070) {
-        $('.map').css('top', '-147px')
-    }else if (screenPosition <= -8071 && screenPosition >= -8200) {
-        $('.map').css('top', '-245px')
-    }else if (screenPosition <= -8201 &&  screenPosition >= -8268) {
-        $('.map').css('top', '-343px')
-    }else if (screenPosition <= -8269 && screenPosition >= -8337) {
-        $('.map').css('top', '-392px')
-    }else if (screenPosition <= -8338 && screenPosition >= -8406) {
-        $('.map').css('top', '-441px')
-    }else if (screenPosition <= -8407 &&  screenPosition >= -8475) {
-        $('.map').css('top', '-490px')
-    }else if (screenPosition <= -8476 && screenPosition >= -8544) {
-        $('.map').css('top', '-539px')
-    }else if (screenPosition <= -8545 && screenPosition >= -8613) {
-        $('.map').css('top', '-588px')
-    }else if (screenPosition <= -8613.1 &&  screenPosition >= -8682) {
-        $('.map').css('top', '-637px')
-    }else if (screenPosition <= -8683 && screenPosition >= -8751) {
-        $('.map').css('top', '-686px')
-    }else if (screenPosition <= -8752 && screenPosition >= -8820) {
-        $('.map').css('top', '-735px')
-    }else if (screenPosition <= -8821 &&  screenPosition >= -8889) {
-        $('.map').css('top', '-784px')
-    }else if (screenPosition <= -8889.1 && screenPosition >= -8958) {
-        $('.map').css('top', '-833px')
-    }else if (screenPosition <= -8959 && screenPosition >= -9027) {
-        $('.map').css('top', '-882px')
-    }else if (screenPosition <= -9028 &&  screenPosition >= -9099) {
-        $('.map').css('top', '-931px')
-    }else if (screenPosition <= -9100 &&  screenPosition >= -12099) {
+    if (screenPosition <= -7760 &&  screenPosition >= -9099) {
+        $('.map').css('top', (value - 35.6) * (-17.25) + '%')
+    }else
+    if (screenPosition <= -9099.1 &&  screenPosition >= -12085) {
         $('.map').css('top', '-978px')
-
-    }else if (screenPosition <= -12100 &&  screenPosition >= -12168) {
-        $('.map').css('top', '-931px')
-    }else if (screenPosition <= -12169 &&  screenPosition >= -12237) {
-        $('.map').css('top', '-882px')
-    }else if (screenPosition <= -12238 && screenPosition >= -12306) {
-        $('.map').css('top', '-833px')
-    }else if (screenPosition <= -12307 && screenPosition >= -12375) {
-        $('.map').css('top', '-784px')
-    }else if (screenPosition <= -12376 &&  screenPosition >= -12454) {
-        $('.map').css('top', '-735px')
-    }else if (screenPosition <= -12455 && screenPosition >= -12513) {
-        $('.map').css('top', '-686px')
-    }else if (screenPosition <= -12514 && screenPosition >= -12582) {
-        $('.map').css('top', '-637px')
-    }else if (screenPosition <= -12583 &&  screenPosition >= -12661) {
-        $('.map').css('top', '-588px')
-    }else if (screenPosition <= -12662 && screenPosition >= -12740) {
-        $('.map').css('top', '-539px')
-    }else if (screenPosition <= -12741 && screenPosition >= -12789) {
-        $('.map').css('top', '-490px')
-    }else if (screenPosition <= -12790 &&  screenPosition >= -12858) {
-        $('.map').css('top', '-441px')
-    }else if (screenPosition <= -12859 && screenPosition >= -12947) {
-        $('.map').css('top', '-392px')
-    }else if (screenPosition <= -12948 && screenPosition >= -13016) { //
-        $('.map').css('top', '-343px')
-    }else if (screenPosition <= -13017 &&  screenPosition >= -13156) {  //
-        $('.map').css('top', '-245px')
-    }else if (screenPosition <= -13157 && screenPosition >= -13276) { //
-        $('.map').css('top', '-147px')
-    }else if (screenPosition <= -13277 && screenPosition >= -13300) { //
-        $('.map').css('top', '-49px')
-    
+    }else
+    if (screenPosition <= -12085.1 &&  screenPosition >= -13420) {
+        $('.map').css('top', ((value - 54.5) * (17.2)) - 100 + '%')
+        // $('.map').css('top', '0px')
     }else{
         $('.map').css('top', '0px')
+    }
+
+
+    // Desert hill
+    var contJump = 0
+    if (screenPosition <= -14595 && screenPosition >= -16150) {
+        $('.main_person').css('top', 'calc(100vh - 510px)')
+        contJump = 1
+    }
+    if (screenPosition <= -14550 && screenPosition >= -15300 && tracking[0] > tracking[1] && contJump == 0) {
+        jumping()
+    }
+    if (screenPosition <= -15300 && screenPosition >= -16235 && tracking[0] < tracking[1] && contJump == 0) {
+        jumping()
+    }
+
+    // Pilot
+    if (screenPosition <= -17270 && screenPosition >= -20730) {
+        $('.main_person').css('visibility', 'hidden')
+        $('.pilot').css({visibility: 'visible', animation: 'pilot 0.5s linear both'})
+        $('.helicopter-propeller-a').css({animation: 'propeller-a 0.1s steps(4) infinite'})
+        $('.helicopter-propeller-b').css({animation: 'propeller-b 0.1s steps(4) infinite'})
+    }else{
+        $('.main_person').css('visibility', 'visible')
+        $('.pilot').css({visibility: 'hidden', animation: 'none'})
+        $('.helicopter-propeller-a').css({animation: 'propeller-a 1s steps(4) infinite'})
+        $('.helicopter-propeller-b').css({animation: 'propeller-b 1s steps(4) infinite'})
+    }
+
+
+    // Fly
+    if (screenPosition <= -17320) {
+        $('.map').css({top: ((value - (71.403)) * 10) + '%', left: '-928.25%'})
+        $('.city-b').css({bottom: ((value - (71.403)) * 5) + '%', left: '38.701%'})
+        $('.helicopter').css({top: 'calc((100vh - 600px) - ' + ((value - (71.403)) * 10) + '%)'})
+    }
+
+    if (screenPosition <= -19950) {
+        $('.map').css({top: '107.5%', left: - ((value * 13) - 140.210) + '%'})
+        $('.helicopter').css({top: 'calc((100vh - 600px) - 106%)', left: 'calc((3500px) + ' + ((value - (82.076)) * 4) + '%)'})
+    }
+
+
+    if (screenPosition <= -20600) {
+        $('.helicopter').css({left: 'calc(3500px + 10.45%)'})
+    }
+
+    if (screenPosition <= -20700) {
+        $('.map').css({top: '100%', left: - ((value * 13) - 140.210) + '%'})
+        $('.helicopter').css({top: 'calc((100vh - 600px) - 104%)', left: 'calc(3500px + 10.45%)'})
     }
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     console.log(screenPosition)
+    console.log(value)
+    console.log(value - 100)
 })
 
-// Main Personage Motion
-setInterval(() => {
-    $('.main_person').css('animation', 'stoping .7s steps(3) infinite')
+const stopping = setInterval(() => {
+    $('.main_person').removeClass('running')
+    $('.main_person').addClass('stopping')
 }, 500)
 
